68cd40b6860b6b7d913d36584a66354ddd78f072
Florian <florian.batt@videomenthe.fr>
2025-06-09 12:49:34 +0200

    Mediatheque: Closes #11 - Adapt user service and model to use UUID

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,17 +1,24 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { UserInput } from '../schemas/userSchema';
+import { v4 as uuidv4 } from 'uuid';
 
 export interface IUser extends Document, UserInput {}
 
 // Mongoose schema for User from zod schema and additional properties
-const UserSchema: Schema = new Schema({
-  nom: { type: String, required: true },
-  prenom: { type: String, required: true },
-  mail: { type: String, required: true, unique: true },
-  telephone: { type: String, required: true },
-  nationalite: { type: String, required: true }
+const UserSchema: Schema = new Schema({                 
+    userId: {
+        type: String,
+        unique: true,
+        default: () => uuidv4(), 
+        immutable: true
+    },
+    nom: { type: String, required: true },
+    prenom: { type: String, required: true },
+    mail: { type: String, required: true, unique: true },
+    telephone: { type: String, required: true },
+    nationalite: { type: String, required: true }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 export default mongoose.model<IUser>('User', UserSchema);
