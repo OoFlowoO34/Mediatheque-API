aa0d2c055a1bc907171cacdbc850704698c9e4c2
Florian <florian.batt@videomenthe.fr>
2025-06-14 23:24:20 +0200

    fix: update MongoDB connection logic to use environment variable or development flag #26

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import { DEVELOPMENT } from './server';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27777/td-mediatheque');
+        await mongoose.connect(DEVELOPMENT? 'mongodb://localhost:27777/td-mediatheque' : "mongodb://mongo:27017/td-mediatheque" );
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
